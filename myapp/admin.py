ae58d32ed5ab1d75f69a50768897af801675ccf0
Phaneendra Gudikandula <phani7.g@gmail.com>
2020-12-12 12:17:04 -0500

    added two features 1)TabularInline for course model 2)Action to reduce price by 10 percent

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from .models import Topic, Course, Student, Order, Review
 
-#changing admin view in admin portal
+
+# changing admin view in admin portal
+class CourseInline(admin.TabularInline):
+    model = Course
+
+
+def reduce_price(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.price = obj.price - ((10 * obj.price) / 100)
+        obj.save()
+
+
 class CourseAdmin(admin.ModelAdmin):
     fields = [('title', 'topic'), ('price', 'num_reviews', 'for_everyone')]
     list_display = ('title', 'topic', 'price')
+    actions = [reduce_price]
 
 
 class OrderAdmin(admin.ModelAdmin):
@@ -12,10 +24,14 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'student', 'order_status', 'order_date', 'total_items')
 
 
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'length')
+    inlines = [CourseInline,
+               ]
 
 
 # Register your models here.
-admin.site.register(Topic)
+admin.site.register(Topic, TopicAdmin)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Student)
 admin.site.register(Order, OrderAdmin)
